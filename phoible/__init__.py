435f571ab3b499af509477a0e21c8528438d9c13
xrotwang <xrotwang@googlemail.com>
2014-07-14 20:45:59 +0200

    have brand link and home menu item

--- phoible/__init__.py
+++ phoible/__init__.py
@@ -1,7 +1,4 @@
-from collections import OrderedDict
-from functools import partial
-
-from clld.web.app import get_configurator, MapMarker, menu_item
+from clld.web.app import get_configurator, MapMarker
 from clld import interfaces, RESOURCES
 from clld.web.adapters.download import N3Dump
 
@@ -35,17 +32,8 @@ def main(global_config, **settings):
         'contribution': '/inventories/view/{id:[^/\.]+}',
     }
     settings['sitemaps'] = ['language', 'source', 'parameter', 'contribution', 'valueset']
-    #settings['navbar.inverse'] = True
     config = get_configurator(
         'phoible', (PhoibleMapMarker(), interfaces.IMapMarker), settings=settings)
-
-
-    menuitems = OrderedDict()
-    for plural in 'contributions languages parameters sources'.split():
-        menuitems[plural] = partial(menu_item, plural)
-    config.registry.registerUtility(menuitems, interfaces.IMenuItems)
-
-
 
     config.include('clldmpg')
     config.include('phoible.maps')
