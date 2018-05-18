f9ed7c1abbeb2c4fe3a00971c1b0a18930aaaa56
chrzyki <christoph@functor-argument.net>
2018-05-18 11:20:49 +0200

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld~=4.2',
-        'clldmpg~=3.1',
+        'clld>=4.2.2',
+        'clldmpg>=3.3.1',
         'pyglottolog>=0.2',
         'clld_glottologfamily_plugin',
         'sqlalchemy',
@@ -30,11 +30,11 @@ setup(
     extras_require={
         'dev': [
             'flake8',
-            'psycopg2',
             'tox',
         ],
         'test': [
             'mock',
+            'psycopg2',
             'pytest>=3.1',
             'pytest-clld',
             'pytest-mock',
